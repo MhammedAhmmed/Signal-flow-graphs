07d9a032f94a885d04aabd312f02f5f61153577e
محمد حسن <nhmdh7092@gmail.com>
2024-04-15 01:46:49 +0200

    controller requests

--- programmingtask2/src/main/java/com/example/programmingtask2/controller.java
+++ programmingtask2/src/main/java/com/example/programmingtask2/controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,8 +17,20 @@ public class controller {
         if (routhHurwitzCriterion.isStable()) {
             return "The system is stable.";
         } else {
-            List<double[]> rightSideRoots = routhHurwitzCriterion.findRightSideRoots();
-            return "The system is unstable. Number of poles in the right-half of the s-plane: " + rightSideRoots ;
+            return "The system is unstable. Number of poles in the right-half of the s-plane: " +routhHurwitzCriterion.findRightSideRoots() ;
         }
+    }
+    @GetMapping("/showRoots")
+    public List<List<Double>> showroots(@RequestParam double[] coefficients) {
+        RouthHurwitzCriterion routhHurwitzCriterion = new RouthHurwitzCriterion(coefficients);
+        List<double[]> roots = routhHurwitzCriterion.findRightSideRoots();
+        List<List<Double>> formattedRoots = new ArrayList<>();
+        for (double[] root : roots) {
+            List<Double> formattedRoot = new ArrayList<>();
+            formattedRoot.add(root[0]); // Real part
+            formattedRoot.add(root[1]); // Imaginary part
+            formattedRoots.add(formattedRoot);
+        }
+        return formattedRoots;
     }
 }
